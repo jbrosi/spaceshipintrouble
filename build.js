827e575ff8ab4ccae5aa2df82fc5b7b1784912d6
Johannes Brosi <johannes.brosi@mayflower.de>
2013-12-21 12:44:22 -0500

    Added cocoon native box2d bindings to index.html and build.js (if they are available). Carefull: The build process includes box2d web, too which is not what we want if we build for the stores (cocoon)

--- build.js
+++ build.js
@@ -10,13 +10,15 @@
         'box2d': { 
             exports: 'Box2D'
         },
+        'box2d-cocoon': {
+           exports: 'Box2D'
+        },
         'stats': {
             exports: 'Stats'
         },
         'hammer': {
             exports: 'Hammer'
         }
-        
     },
     paths: {           
         stats: 'lib/stats',
@@ -24,11 +26,12 @@
         'helvetiker-font': '../assets/fonts/helvetiker_regular.typeface',
 		lodash: 'lib/lodash',
 		box2d: 'lib/box2dweb',
+		'box2d-cocoon': 'lib/box2d_cocoonJS',
 		require: 'lib/require',
 		hammer: 'lib/hammer'
     },
     name: "lib/almond",
-    include: ["app", "game/screens/game-menu", "game/screens/main-menu", "game/screens/settings-menu", "game/screens/standard-level"],
+    include: ["app", "game/screens/game-menu", "game/screens/main-menu", "game/screens/settings-menu", "game/screens/standard-level", 'box2d', 'box2d-cocoon'],
     insertRequire: ["app"],
     out: "main-built.js"
 })
--- js/helper/box2DHelper.js
+++ js/helper/box2DHelper.js
@@ -1,28 +1,31 @@
 
 
-define(['box2d'], function (Box2D) {
+define(
+    [navigator.isCocoonJS ? 'box2d-cocoon' : 'box2d'], 
+    function (Box2D) {
 
-    //just add some shortcuts
-    Box2D.b2Vec2 = Box2D.Common.Math.b2Vec2;
-    Box2D.b2AABB = Box2D.Collision.b2AABB;
-    Box2D.b2BodyDef = Box2D.Dynamics.b2BodyDef;
-    Box2D.b2Body = Box2D.Dynamics.b2Body;
-    Box2D.b2FixtureDef = Box2D.Dynamics.b2FixtureDef;
-    Box2D.b2Fixture = Box2D.Dynamics.b2Fixture;
-    Box2D.b2World = Box2D.Dynamics.b2World;
-    Box2D.b2MassData = Box2D.Collision.Shapes.b2MassData;
-    Box2D.b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape;
-    Box2D.b2CircleShape = Box2D.Collision.Shapes.b2CircleShape;
-    Box2D.b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
-    Box2D.b2MouseJointDef =  Box2D.Dynamics.Joints.b2MouseJointDef;
-      
-      
-    //missing functions in box2dweb
-    Box2D.b2Body.prototype.ApplyAngularImpulse = function(impulse) {
-        if (this.IsAwake() == false) {
-            this.SetAwake(true);
-        }
-        this.m_angularVelocity += this.m_invI * impulse;
-    };
-    return Box2D;
-});
+        //just add some shortcuts
+        Box2D.b2Vec2 = Box2D.Common.Math.b2Vec2;
+        Box2D.b2AABB = Box2D.Collision.b2AABB;
+        Box2D.b2BodyDef = Box2D.Dynamics.b2BodyDef;
+        Box2D.b2Body = Box2D.Dynamics.b2Body;
+        Box2D.b2FixtureDef = Box2D.Dynamics.b2FixtureDef;
+        Box2D.b2Fixture = Box2D.Dynamics.b2Fixture;
+        Box2D.b2World = Box2D.Dynamics.b2World;
+        Box2D.b2MassData = Box2D.Collision.Shapes.b2MassData;
+        Box2D.b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape;
+        Box2D.b2CircleShape = Box2D.Collision.Shapes.b2CircleShape;
+        Box2D.b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
+        Box2D.b2MouseJointDef =  Box2D.Dynamics.Joints.b2MouseJointDef;
+          
+          
+        //missing functions in box2dweb
+        Box2D.b2Body.prototype.ApplyAngularImpulse = function(impulse) {
+            if (this.IsAwake() == false) {
+                this.SetAwake(true);
+            }
+            this.m_angularVelocity += this.m_invI * impulse;
+        };
+        return Box2D;
+    }
+);
