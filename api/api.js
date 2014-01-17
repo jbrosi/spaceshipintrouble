d302596156bd134e48d85d248332288313c5225f
Johannes Brosi <mail@jbrosi.de>
2014-01-17 13:32:12 +0100

    closes #17. Added API Doc to github pages /api

---
+++ api/api.js
@@ -0,0 +1,32 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "engine.GameInitializer",
+        "engine.entity.Entity",
+        "engine.entity.EntityComponent",
+        "engine.entity.EntityManager",
+        "engine.entity.EntityMessage",
+        "engine.entity.EntityPrototype",
+        "engine.entity.EntityScript",
+        "engine.helper.Box2DHelper",
+        "engine.helper.KeyboardHelper",
+        "engine.helper.PerformanceTimer",
+        "engine.helper.StatsHelper",
+        "engine.helper.TextHelper",
+        "engine.helper.VirtualJoystick",
+        "engine.map.AbstractMapLoader",
+        "engine.map.MapLayer",
+        "engine.map.MapObject",
+        "engine.map.TileSet",
+        "engine.map.TiledJSONMapLoader",
+        "engine.map.TiledMap",
+        "engine.screens.AbstractScreen",
+        "engine.screens.LoadLevelScreen",
+        "engine.screens.PlayLevelScreen",
+        "engine.screens.Screenmanager",
+        "engine.util.ResourceLoader"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
