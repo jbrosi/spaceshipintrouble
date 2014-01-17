decb66bdfd6a6b98668a6f1c5bd65e748c4f6b1b
Johannes Brosi <mail@jbrosi.de>
2014-01-17 12:57:02 +0100

    worked on #17. Complete docblocks for the engine.util namespace

--- js/engine/util/resourceLoader.ts
+++ js/engine/util/resourceLoader.ts
@@ -6,26 +6,51 @@ import Q = require('q');
 //this is the timeout for loading the resources
 var DEFAULT_TIMEOUT = 6000;
 
+/**
+ * The ResourceLoader helps you loading various resources with promises.
+ *
+ * Warning: SINGLETON! Use `getInstance()` instead of `new`
+ *
+ * @namespace engine.util
+ * @class ResourceLoader
+ */
 class ResourceLoader {
     
     private static _instance;
-    
+
+    /**
+     * Warning: SINGLETON. Don't use this constructor, use `getInstance()` instead
+     *
+     * @method __constructor
+     */
     constructor() {
       if (ResourceLoader._instance){
             throw new Error("Error: Instantiation failed: Use ResourceLoader.getInstance() instead of new.");
         }
         ResourceLoader._instance = this;    
     }
-    
+
+    /**
+     * Singleton method for getting the instance for this `ResourceLoader`
+     *
+     * @method getInstance
+     * @static
+     * @returns {engine.util.ResourceLoader} the singleton instance of this `ResourceLoader`
+     */
     public static getInstance(): ResourceLoader {
         if (ResourceLoader._instance == null) {
             ResourceLoader._instance = new ResourceLoader();
         }
         return ResourceLoader._instance;
     }
-    
-    
-    public loadJSONFile(jsonFile: String) {
+
+    /**
+     * Loads and parses a json file and returns a promise to it
+     *
+     * @param jsonFile the file to load
+     * @returns {Promise(*)} promise for the parsed JSON Data
+     */
+    public loadJSONFile(jsonFile: string) {
         var deferred = Q.defer();
         $.ajax({
             type: 'GET',
@@ -40,10 +65,7 @@ class ResourceLoader {
         });
         return deferred.promise;
     }
-    
-    
-    public loadTextFile(textFile: String) {
-    }
+
 };
 
 
