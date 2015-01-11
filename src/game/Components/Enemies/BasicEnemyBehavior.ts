07e6fd7cdc78375952174106af98d1052b7aea7e
Johannes Brosi <me@brosi.me>
2015-01-11 12:53:05 +0100

    worked on #8: Enemies now try to look the player in the eyes :)

--- src/game/Components/Enemies/BasicEnemyBehavior.ts
+++ src/game/Components/Enemies/BasicEnemyBehavior.ts
@@ -45,7 +45,7 @@ module SpaceshipInTrouble.Game.Components.Enemies {
 
             var direction = posTarget.sub(pos);
             var distance = direction.length();
-            if (distance < 60  && distance > 20) {
+            if (distance < 60 && distance > 20) {
                 this._isActive = true;
             } else {
                 this._isActive = false;
@@ -57,13 +57,15 @@ module SpaceshipInTrouble.Game.Components.Enemies {
                 //it's completely strange -> we need to set the force point to a value where no enemy is o_O if we use the
                 //enemies coordinates it moves slowly and crushes through walls... Maybe I don't have yet understood the
                 //real meaning of this point... well but it seems to work when setting to somewhat wide outside the level o_O
-                this._physicBody.ApplyForce(new Box2D.Common.Math.b2Vec2(force.x, force.y), new Box2D.Common.Math.b2Vec2(99999,99999));
+                this._physicBody.ApplyForce(new Box2D.Common.Math.b2Vec2(force.x, force.y), new Box2D.Common.Math.b2Vec2(99999, 99999));
 
             }
 
+            if (distance < 60) {
+                //look the player in the eyes!
+                this.getEntity().getObject3D().setRotationFromAxisAngle(new THREE.Vector3(0,0,1), Math.PI/2 + Math.atan2(direction.x, -direction.y));
+            }
 
         }
-
-
     }
 }
